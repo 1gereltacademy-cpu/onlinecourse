dbd8b8ad52376f0d0df76953cec0f0576924801a
1gereltacademy-cpu <1gereltacademy@gmail.com>
2026-04-13 23:20:43 +0800

    nm

--- app/admin/users/page.jsx
+++ app/admin/users/page.jsx
@@ -14,7 +14,7 @@ async function getAdminSupabase() {
 
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_ROLE_KEY,
     {
       global: {
         headers: {
